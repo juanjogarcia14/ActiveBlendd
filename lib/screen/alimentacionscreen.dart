4534ab167e7f016a691f4b943492280ebf1151a4
juanjogr14 <juanjogr14@hotmail.com>
2025-01-30 13:27:39 +0100

    Primer commit del proyecto Flutter ActiveBlendd

---
+++ lib/screen/alimentacionscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AlimentacionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Alimentación")),
+      body: Center(child: Text("Pantalla de Alimentación active blend")),
+    );
+  }
+}
