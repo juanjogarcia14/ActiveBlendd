22724746846dd85aa0a64223abc54955dd564790
Iván Tormo Valero <ivaanvaleroo@gmail.com>
2025-01-31 18:29:01 +0100

    Pantalla ropa deportiva completa a falta de firebase con menu hamburguesa y carrito

---
+++ lib/screen/materialscreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MaterialScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Materiales")),
+      body: Center(child: Text("Pantalla de Materiales")),
+    );
+  }
+}
---
+++ lib/screen/ropa_deportiva_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RopaDeportivaScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Ropa Deportiva")),
+      body: Center(child: Text("Pantalla de Ropa Deportiva")),
+    );
+  }
+}
